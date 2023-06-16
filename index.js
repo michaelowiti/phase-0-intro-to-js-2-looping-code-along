476d2b892278cbb3e0caafeabf03dfe07f591558
mike <mjowiti@gmail.com>
2023-06-16 10:13:15 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const arrayString = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(arrayString,eventNames){
+     const messages=[]
+    for(let i=0;i<arrayString.length;i++){
+        messages.push(`Thank you, ${arrayString[i]}, for the wonderful ${eventNames} gift!`);
+            }
+    return messages
+}
+writeCards(["Guadalupe", "Ollie", "Aki"],"surprise")
+const messages=[]
+function countDown(int){
+    let i=int; while(i>=0) {int--; }
+    console.log(int)}
+
+    console.log(int)
+
+
+countDown(4)
